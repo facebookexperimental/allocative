0117b1828de223a4393b5f2a8853c9f9b9dc7a56
Alexander Mols <asm@meta.com>
2022-11-08 02:14:01 -0800

    Mark fbcode toolchains as rust_nightly
    Summary:
    Fbcode toolchains support nightly features. The allocative build
    script checks `rustc --version` for the presence of the nightly substring, but
    the fbcode toolchain won't have it in its output.
    
    Add an additional check to see if the sysroot for rustc contains `platform010`,
    indicating that the fbcode platform is used and nightly features can be
    enabled.
    
    Reviewed By: ndmitchell, krallin
    
    Differential Revision: D41117401
    
    fbshipit-source-id: c4fe06ce1d774afa49c821a558f97c93a817c330

--- allocative/build.rs
+++ allocative/build.rs
@@ -4,6 +4,14 @@ fn main() {
 
 fn rust_nightly() {
     let rustc = std::env::var("RUSTC").unwrap();
+
+    let nightly = version_is_nightly(&rustc) || sysroot_is_meta_internal_rustc(&rustc);
+    if nightly {
+        println!("cargo:rustc-cfg=rust_nightly");
+    }
+}
+
+fn version_is_nightly(rustc: &str) -> bool {
     let version = std::process::Command::new(rustc)
         .arg("--version")
         .output()
@@ -18,8 +26,30 @@ fn rust_nightly() {
 
     let stdout = String::from_utf8(version.stdout).unwrap();
     assert!(stdout.contains("rustc"), "Sanity check");
-    let nightly = stdout.contains("nightly");
-    if nightly {
-        println!("cargo:rustc-cfg=rust_nightly");
-    }
+    stdout.contains("nightly")
+}
+
+/// Meta only.
+///
+/// Check for fbcode platform based rustc. These won't have nightly in their version string, but
+/// have nightly features enabled anyway.
+///
+/// The path to RUSTC will be set to just "rustc" when using rustup. We can use the location of
+/// sysroot to check if it points at a platform.
+fn sysroot_is_meta_internal_rustc(rustc: &str) -> bool {
+    let sysroot = std::process::Command::new(rustc)
+        .arg("--print")
+        .arg("sysroot")
+        .output()
+        .unwrap();
+
+    assert!(sysroot.status.success());
+
+    // Fbcode platform output:
+    // /mnt/gvfs/third-party2/rust/60b8aa4c3d91d38d16a232de8079c2e75e4e6304/1.64.0/platform010/4eb61b6
+    // Stable output:
+    // /home/asm/.rustup/toolchains/stable-x86_64-unknown-linux-gnu
+
+    let stdout = String::from_utf8(sysroot.stdout).unwrap();
+    stdout.contains("platform010")
 }
